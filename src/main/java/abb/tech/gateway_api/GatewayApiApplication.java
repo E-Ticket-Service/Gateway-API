be9c2532fd0d7292d96ed8f21f2bf268f8247a92
ayla.maharramova <maharramova2000@gmail.com>
2026-03-17 07:44:38 -0400

    Refactor and update project

--- src/main/java/abb/tech/gateway_api/GatewayApiApplication.java
+++ src/main/java/abb/tech/gateway_api/GatewayApiApplication.java
@@ -2,6 +2,8 @@ package abb.tech.gateway_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import abb.tech.gateway_api.security.GatewaySecurityProperties;
 
 @SpringBootApplication
 public class GatewayApiApplication {
